6465cdb085998dae62c3093c86151a1617012d22
NyckdeVries21 <michaelwitkamp16@gmail.com>
2025-11-14 09:44:49 +0100

    lightning bounce ik heb aids

--- Mini-Mages/Assets/Devs/Scripts/LightningAttack.cs
+++ Mini-Mages/Assets/Devs/Scripts/LightningAttack.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class LightningAttack : Attack
 {
@@ -6,8 +7,9 @@ public class LightningAttack : Attack
 
     public float WallsBounced;
     public float BounceLimit = 2;
-
     private Rigidbody rb;
+
+    Vector3 lastVelocity;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,22 +18,39 @@ public class LightningAttack : Attack
 
     private void Update()
     {
-        rb.AddForce(transform.forward * ObjectSpeed);
+        rb.AddForce(transform.forward * ObjectSpeed); // object speed
+        lastVelocity = rb.linearVelocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+            rb.linearVelocity = direction * Mathf.Max(speed, 0f);
+
             WallsBounced++;
-            rb.AddForce(-transform.forward * ObjectSpeed);
+            //transform.Rotate(new Vector3(0, 180f, 0));
+            //transform.position = Vector3.Reflect(transform.position, transform.forward);
+            
+
             if (WallsBounced >= BounceLimit)
             {
                 // iets van verwijderen of dat ie niet meer door bounced
-            } 
+            }
 
             //als ie de player raakt, dan gaat ie kapot
-        }
-    }
 
+        }
+
+
+        //test if statement
+        if (collision.gameObject.CompareTag("kutjebef"))
+        {
+
+        }
+
+    }
 }
