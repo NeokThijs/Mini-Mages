b58edadd96cd58b3ce8780685f7bd4bb723bcfe1
NyckdeVries21 <michaelwitkamp16@gmail.com>
2025-11-24 13:04:40 +0100

    na de pauze push

--- Mini-Mages/Assets/Devs/Scripts/GameManager.cs
+++ Mini-Mages/Assets/Devs/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,23 +11,48 @@ public class GameManager : MonoBehaviour
     [SerializeField] private List<Transform> SpawnpointsPlayers;
     [SerializeField] private float PlayerAmount = 4;
 
-    // testUI
-    [SerializeField] private TMPro.TextMeshPro roundText;
+    [Header("UI (Settings)")]
+    [SerializeField] private TMPro.TextMeshProUGUI roundText;
     private float roundAmount = 3;
-    private bool isRoundActive = true;
+    private bool isRoundActive = false;
 
+    [SerializeField] private GameObject StartUI;
+    [SerializeField] private TMPro.TMP_InputField playerAmountIF; // IF = inputfield
+    public float removeStartUI = 0;
+    
     void Start()
     {
-       
-        SpawnPlayers();
+        StartUI.SetActive(true);
+        Time.timeScale = 0f; // stop the game  
     }
 
     void Update()
     {
-        if (isRoundActive && playersInGame.Count <= 1)
+        if (isRoundActive && playersInGame.Count == 1)
         {
             StartNewRound();
         }
+
+        removeStartUI += Time.unscaledDeltaTime; // timer voor geen pauze dr in
+
+        if (removeStartUI >= 12)
+        {
+            StartUI.SetActive(false);
+            Time.timeScale = 1f; // continue game
+
+            if (!isRoundActive)
+            {
+                StartNewRound();
+                isRoundActive = true;
+            }
+        }
+
+    }
+
+    public void UIPlayerAmount()
+    {
+        int UIPlayerAmount = int.Parse(playerAmountIF.text);
+        PlayerAmount = UIPlayerAmount;
     }
 
     private void StartNewRound()
@@ -40,6 +66,7 @@ public class GameManager : MonoBehaviour
         // zoekt de objecten met de tag player
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         playersInGame.AddRange(players);
+        Debug.Log("Voegt player toe a/d list");
     }
 
     private void SpawnPlayers()
@@ -47,6 +74,7 @@ public class GameManager : MonoBehaviour
         for ( int i = 0; i < PlayerAmount; i++)
         {
             Instantiate(player, SpawnpointsPlayers[Random.Range(0, SpawnpointsPlayers.Count)].transform.position, Quaternion.identity);
+            Debug.Log("Spawn players");
         }
 
         
