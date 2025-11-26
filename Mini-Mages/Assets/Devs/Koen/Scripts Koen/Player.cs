6e7d3c019316b1b80f233f70da5415c178ec3186
NeokThijs <koen.abbink@ziggo.nl>
2025-11-26 11:41:24 +0100

    animation start

--- Mini-Mages/Assets/Devs/Koen/Scripts Koen/Player.cs
+++ Mini-Mages/Assets/Devs/Koen/Scripts Koen/Player.cs
@@ -24,8 +24,11 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         dashBrain = GetComponent<Dash>();
+        if (GnomeHat != null)
+        { 
         MeshRenderer = GnomeHat.GetComponent<MeshRenderer>();
         MeshRenderer.material = colors[playerInputObject.playerIndex];
+        }
         Collider = GetComponent<Collider>();
         Collider.excludeLayers = 1 << LayerMask.NameToLayer(gameObject.tag + "Attack");
         rb.excludeLayers = 1 << LayerMask.NameToLayer(gameObject.tag + "Attack");
