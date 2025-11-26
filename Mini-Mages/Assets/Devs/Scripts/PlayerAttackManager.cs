6e6c2d84583e4cd7009d3d028fe3338c858decd1
NeokThijs <koen.abbink@ziggo.nl>
2025-11-26 09:46:01 +0100

    woooooooww, shit werkt

--- Mini-Mages/Assets/Devs/Scripts/PlayerAttackManager.cs
+++ Mini-Mages/Assets/Devs/Scripts/PlayerAttackManager.cs
@@ -8,12 +8,18 @@ public class PlayerAttackManager : MonoBehaviour
     [SerializeField] private GameObject Wack;
     [SerializeField] public GameObject SpecialAttack;
     [SerializeField] private Transform PlaceAttack;
+    [SerializeField] private float attackCooldown;
+    private float currentAttackCooldown;
     
     public float AttackAmount;
 
     private void Update()
     {
         CheckAttack();
+        if(currentAttackCooldown >= 0f)
+        {
+            currentAttackCooldown -= Time.deltaTime;
+        }
     }
     private void CheckAttack() // kijkt of er een special attack is, en of die nog niet op is
     {
@@ -34,42 +40,27 @@ public class PlayerAttackManager : MonoBehaviour
     {
         // gebruik attack / spawnen
         // telt 1 ervanaf, als ie tot 0 is dan verwijderen
-        if (context.performed)
+        if (context.performed && currentAttackCooldown <= 0)
         {
-        if (SpecialAttack != null && AttackAmount > 0)
-        {
-            GameObject Attack = Instantiate(SpecialAttack, PlaceAttack.position, PlaceAttack.rotation);
-                Attack.layer = LayerMask.NameToLayer(gameObject.tag + "Attack"); //zet de layer van de attack naar de player tag + attack
-                Attack.transform.parent = null; //remove the parent of the attack
-                Debug.Log("special attack gebruikt");
-            AttackAmount --; //attack charges -1
-            Debug.Log(AttackAmount + "charges left");
-        }
-        else if (SpecialAttack == null)
-        {
-            Instantiate(Wack, PlaceAttack.position, PlaceAttack.rotation);
-        }
+            startCooldown();
+            if (SpecialAttack != null && AttackAmount > 0)
+            {
+                GameObject Attack = Instantiate(SpecialAttack, PlaceAttack.position, PlaceAttack.rotation);
+                    Attack.layer = LayerMask.NameToLayer(gameObject.tag + "Attack"); //zet de layer van de attack naar de player tag + attack
+                    Attack.transform.parent = null; //remove the parent of the attack
+                    Debug.Log("special attack gebruikt");
+                AttackAmount --; //attack charges -1
+                Debug.Log(AttackAmount + "charges left");
+            }
+            else if (SpecialAttack == null)
+            {
+                Instantiate(Wack, PlaceAttack.position, PlaceAttack.rotation);
+            }
         }
     }
-
-    private void OnCollisionEnter(Collision collision)
+    private void startCooldown()
     {
-        // de pickup oppakken die een tag heeft van pickup
-        // attack v/d pickup word in de special attack dr ingezet
-        //if (collision.gameObject.CompareTag("Pickup") == false)
-        //{
-        //    return;
-        //}
-        //else if (collision.gameObject.CompareTag("Pickup"))
-        //{
-        //    Pickup pickupScript = collision.gameObject.GetComponent<Pickup>();
-        //    if (SpecialAttack != null)
-        //    {
-        //        SpecialAttack = pickupScript.AttackObject;
-        //        Debug.Log("attack vervangen");
-        //    }
-
-        //}
+        currentAttackCooldown = attackCooldown;
     }
 
 }
